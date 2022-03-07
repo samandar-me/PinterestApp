7d2dce63e817f0467fee15b36792e21314545061
Samandar <samandarcoderdev@gmail.com>
2022-03-07 19:03:36 +0500

    fragments

--- app/src/main/java/uz/context/pinterestapp/activities/MainActivity.kt
+++ app/src/main/java/uz/context/pinterestapp/activities/MainActivity.kt
@@ -1,11 +1,15 @@
-package uz.context.pinterestapp
+package uz.context.pinterestapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import uz.context.pinterestapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
