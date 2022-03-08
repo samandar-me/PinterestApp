c9890c2c541ba179bb9c91800636be88a33971a2
ConaMobile <conamobiledev@gmail.com>
2022-03-08 15:23:37 +0500

    kop narsa

--- app/src/main/java/uz/context/pinterestapp/activities/MainActivity.kt
+++ app/src/main/java/uz/context/pinterestapp/activities/MainActivity.kt
@@ -2,12 +2,16 @@ package uz.context.pinterestapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import uz.context.pinterestapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //deleter night mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
     }
 }
